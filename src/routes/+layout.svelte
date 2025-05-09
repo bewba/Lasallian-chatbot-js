8b811d3fbb9cfa9ade9c216e786c474385275a44
bewba <lumaguihans0123@gmail.com>
2025-05-10 02:16:16 +0800

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	import '../app.css';
 
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
 </script>
 
